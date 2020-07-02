7ab74c8afe3f0859557511f873e56da7a9e90c3e
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-02 15:59:15 +0100

    reimplement bit population check to return bool

--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,5 @@
-unsigned int select_bit(unsigned int position, unsigned int bit_vector) {
-    return bit_vector & (1 << position);
+#include <linux/types.h>
+
+bool utils_is_bit_set(u64 position, u64 bit_vector) {
+    return (bool)(bit_vector & (1 << position));
 }
