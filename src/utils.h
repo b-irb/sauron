759578cd5ed945d2e0b570472ce0409fe1b98575
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-02 21:40:23 +0100

    allow for format specifiers and remove redefinition warning

--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,11 @@
-#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include <linux/printk.h>
 #include <linux/types.h>
 
-#define utils_log(level, msg) pr_##level(msg);
+#ifdef pr_fmt
+#undef pr_fmt
+#endif
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#define utils_log(level, msg, ...) pr_info(msg, ##__VA_ARGS__);
 
 bool utils_is_bit_set(u64, u64);
