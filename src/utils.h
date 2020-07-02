0ae76ddfdb71b9d982f9e07858dfb1b079d52357
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-02 16:00:01 +0100

    implement logging alias

--- src/utils.h
+++ src/utils.h
@@ -1,1 +1,8 @@
-unsigned int select_bit(unsigned int, unsigned int);
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/printk.h>
+#include <linux/types.h>
+
+#define utils_log(level, msg) pr_##level(msg);
+
+bool utils_is_bit_set(u64, u64);
