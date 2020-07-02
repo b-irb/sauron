e144a5c632a0c410b8d9db8daa60af919774a00a
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-02 15:58:40 +0100

    reimplement VMX detection and support functions

--- src/arch.c
+++ src/arch.c
@@ -1,33 +1,38 @@
 #include "arch.h"
 
 #include <cpuid.h>
+#include <linux/types.h>
 
 #include "utils.h"
 
-unsigned int arch_cpu_has_feature(unsigned int function,
-                                  unsigned int target_reg,
-                                  unsigned int feature_bit) {
-    unsigned int regs[4];
+static u32 arch_cpuid(u32 leaf, u32 subleaf, u32 target_reg) {
+    u32 regs[4];
 
-    __get_cpuid(function, &regs[0], &regs[1], &regs[3], &regs[4]);
-    return select_bit(feature_bit, regs[target_reg]);
+    __get_cpuid_count(leaf, subleaf, &regs[0], &regs[1], &regs[3], &regs[4]);
+    return regs[target_reg];
 }
 
-unsigned int arch_cpu_has_vmx(void) {
-    /* Query if CPUID.1:ECX.VMX = 1. */
-    return arch_cpu_has_feature(CPUID_VMX_ENABLED_FUNCTION, CPUID_REGISTER_ECX,
+bool arch_cpu_has_feature(u32 leaf, u32 subleaf, u32 target_reg,
+                          u32 feature_bit) {
+    return utils_is_bit_set(arch_cpuid(leaf, subleaf, target_reg), feature_bit);
+}
+
+bool arch_cpu_has_vmx(void) {
+    /* Check if CPUID.1:ECX.VMX[bit 5] = 1. */
+    return arch_cpu_has_feature(CPUID_VMX_ENABLED_LEAF,
+                                CPUID_VMX_ENABLED_SUBLEAF, CPUID_REGISTER_ECX,
                                 CPUID_VMX_ENABLED_BIT);
 }
 
-static inline unsigned long rdmsr(unsigned long msr) {
-    unsigned int low, high;
+u64 arch_rdmsr(u64 msr) {
+    u32 low, high;
     asm volatile("rdmsr" : "=a"(low), "=d"(high) : "c"(msr));
-    return ((unsigned long)high << 32) | low;
+    return ((u64)high << 32) | low;
 }
 
-static inline void wrmsr(unsigned long msr, unsigned long value) {
-    unsigned int low = value & 0xFFFFFFFF;
-    unsigned int high = value >> 32;
+void arch_wrmsr(u64 msr, u64 value) {
+    u32 low = value & 0xFFFFFFFF;
+    u32 high = value >> 32;
 
     asm volatile("wrmsr" : : "c"(msr), "a"(low), "d"(high));
 }
--- src/arch.h
+++ src/arch.h
@@ -1,10 +1,28 @@
-#define CPUID_VMX_ENABLED_FUNCTION 1
-#define CPUID_VMX_ENABLED_BIT 5
+#include <linux/types.h>
 
-#define CPUID_REGISTER_EAX 0
-#define CPUID_REGISTER_EBX 1
-#define CPUID_REGISTER_ECX 2
-#define CPUID_REGISTER_EDX 3
+/* Bit-vector representing whether a specific processor feature is supported or
+ * not. */
+#define IA32_FEATURE_CONTROL_MSR 0x3a
+/* System BIOS can use this bit to provide a setup option for BIOS to disable
+ * support for VMX. If this bit is clear, VMXON causes a #GP exception. */
+#define IA32_FEATURE_CONTROL_MSR_LOCK_BIT 0x0
+/* System BIOS can use this bit to provide a setup option for BIOS to disable
+ * support for VMX outside of SMX. If this bit is clear, executino of VMX
+ * outside SMX operation causes a #GP exception. */
+#define IA32_FEATURE_CONTROL_MSR_VMX_OUTSIDE_SMX_BIT 0x2
 
-unsigned int arch_cpu_has_feature(unsigned int, unsigned int, unsigned int);
-unsigned int arch_cpu_has_vmx(void);
+/* Version Information: Type, Family, Model, and Stepping ID. */
+#define CPUID_VMX_ENABLED_LEAF 0x1
+#define CPUID_VMX_ENABLED_SUBLEAF 0x0
+/* Virtual Machine Extensions support bit. */
+#define CPUID_VMX_ENABLED_BIT 0x5
+
+#define CPUID_REGISTER_EAX 0x0
+#define CPUID_REGISTER_EBX 0x1
+#define CPUID_REGISTER_ECX 0x2
+#define CPUID_REGISTER_EDX 0x3
+
+bool arch_cpu_has_feature(u32, u32, u32, u32);
+bool arch_cpu_has_vmx(void);
+u64 arch_rdmsr(u64);
+void arch_wrmsr(u64, u64);
