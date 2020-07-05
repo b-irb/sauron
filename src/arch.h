494e2226afdbd84da6d0e9058d95a9d83f76b945
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-05 14:54:25 +0100

    prefer __rdmsr and __wrmsr intrinsics into defined structures

--- src/arch.h
+++ src/arch.h
@@ -1,16 +1,5 @@
 #include <linux/types.h>
 
-/* Bit-vector representing whether a specific processor feature is supported or
- * not. */
-#define IA32_FEATURE_CONTROL_MSR 0x3a
-/* System BIOS can use this bit to provide a setup option for BIOS to disable
- * support for VMX. If this bit is clear, VMXON causes a #GP exception. */
-#define IA32_FEATURE_CONTROL_MSR_LOCK_BIT 0x0
-/* System BIOS can use this bit to provide a setup option for BIOS to disable
- * support for VMX outside of SMX. If this bit is clear, executino of VMX
- * outside SMX operation causes a #GP exception. */
-#define IA32_FEATURE_CONTROL_MSR_VMX_OUTSIDE_SMX_BIT 0x2
-
 /* Version Information: Type, Family, Model, and Stepping ID. */
 #define CPUID_VMX_ENABLED_LEAF 0x1
 #define CPUID_VMX_ENABLED_SUBLEAF 0x0
@@ -24,5 +13,3 @@
 
 bool arch_cpu_has_feature(u32, u32, u32, u32);
 bool arch_cpu_has_vmx(void);
-u64 arch_rdmsr(u64);
-void arch_wrmsr(u64, u64);
