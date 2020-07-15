1615f4d4b80c1b89123a4cdf065a6025d9c52cdd
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-15 15:44:55 +0100

    allocate 2 pages per VMCS related structure

--- src/vmcs.h
+++ src/vmcs.h
@@ -5,12 +5,12 @@
 
 #include "ia32.h"
 
+/* Maximum size for VMCS related structures is 4096 bytes (1 standard page). */
 #define VMCS_REGION_REQUIRED_PAGES 2
 #define VMCS_REGION_REQUIRED_BYTES PAGE_SIZE* VMCS_REGION_REQUIRED_PAGES
 
 struct vmm_ctx;
 struct cpu_ctx;
 
-VMCS* hv_vmcs_vmcs_alloc(struct cpu_ctx*);
+VMCS* hv_vmcs_vmcs_create(struct cpu_ctx*);
 void hv_vmcs_vmcs_destroy(VMCS*);
-ssize_t hv_vmcs_vmcs_init(struct cpu_ctx*);
--- src/vmx.h
+++ src/vmx.h
@@ -6,12 +6,13 @@
 #include "ia32.h"
 
 /* Maximum size for VMCS related structures is 4096 bytes (1 standard page). */
-#define VMXON_REGION_REQUIRED_PAGES 1
+#define VMXON_REGION_REQUIRED_PAGES 2
 #define VMXON_REGION_REQUIRED_BYTES PAGE_SIZE* VMXON_REGION_REQUIRED_PAGES
 
 struct cpu_ctx;
 
+void hv_vmx_launch_cpu(struct cpu_ctx*);
+ssize_t hv_vmx_enter_root(struct cpu_ctx*);
+ssize_t hv_vmx_exit_root(struct cpu_ctx*);
 VMXON* hv_vmx_vmxon_create(struct cpu_ctx*);
 void hv_vmx_vmxon_destroy(VMXON*);
-void hv_vmx_set_fixed_bits(struct cpu_ctx*);
-void hv_vmx_reset_fixed_bits(struct cpu_ctx*);
