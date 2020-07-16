aaa14a77c4d565c538ba4b21cdffc9c13b938c57
birb007 <25516647+birb007@users.noreply.github.com>
2020-07-16 14:55:22 +0100

    add MSR bitmap and reorganise struct

--- src/cpu.h
+++ src/cpu.h
@@ -12,16 +12,22 @@ struct cpu_ctx {
     unsigned processor_id;
     bool failed;
 
-    struct hv_arch_cpu_state state;
-    u64 resume_sp;
-    u64 resume_ip;
-    VMXON* vmxon_region;
-    phys_addr_t vmxon_region_ptr;
-    VMCS* vmcs_region;
-    phys_addr_t vmcs_region_ptr;
-
     CR0 unfixed_cr0;
     CR4 unfixed_cr4;
+
+    struct hv_arch_cpu_state state;
+
+    VMX_MSR_BITMAP* msr_bitmap;
+    void* vmexit_handler;
+    void* vmexit_handler_stack;
+
+    u64 resume_sp;
+    u64 resume_ip;
+
+    VMXON* vmxon_region;
+    VMCS* vmcs_region;
+    phys_addr_t vmxon_region_ptr;
+    phys_addr_t vmcs_region_ptr;
 };
 
 ssize_t hv_cpu_ctx_init(struct cpu_ctx*, struct vmm_ctx*);
