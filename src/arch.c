26d15c60fb57246d9947233f8b9cf199f7be6e0a
birb007 <25516647+birb007@users.noreply.github.com>
2020-08-13 21:36:05 +0100

    fix segment descriptors and assembly routines
    Segment selectors were not properly validated as system descriptors.
    Additionally, `hv_arch_lar` was not loading the correct access rights
    which caused issues with `VMLAUNCH`.
    
    `VMXOFF` will set `ZF` if there is a valid VMCS which we now account
    for.

--- src/arch.c
+++ src/arch.c
@@ -3,11 +3,12 @@
 #include <asm/desc.h>
 #include <asm/desc_defs.h>
 #include <asm/io.h>
+#include <asm/paravirt.h>
 #include <asm/processor-flags.h>
 #include <asm/processor.h>
 #include <asm/ptrace.h>
 #include <asm/special_insns.h>
-#include <cpuid.h>
+#include <asm/tlbflush.h>
 #include <linux/sched/task_stack.h>
 #include <linux/types.h>
 
@@ -15,47 +16,40 @@
 #include "ia32.h"
 #include "utils.h"
 
-#define PARSE_SELECTOR(cpu, selector)                           \
-    hv_arch_read_seg_descriptor(&cpu_ctx->state.seg_##selector, \
-                                cpu_ctx->state.gdtr,            \
-                                (SEGMENT_SELECTOR)hv_arch_read_##selector());
+#define OUTPUT_SEG_DESC(SEG, desc) \
+    hv_utils_cpu_log(                                                       \
+        debug,                                                               \
+        TOSTRING(                                                           \
+            SEG) " selector=%x, base=%pK, limit=%llx, access_rights=%x, unusable=%d\n", \
+        desc.selector.flags, (void*)desc.base_address, desc.limit,                 \
+        desc.access_rights.flags, desc.access_rights.unusable);
 
-static u32 arch_cpuid(u32 leaf, u32 subleaf, u32 target_reg) {
-    u32 regs[4];
-
-    __get_cpuid_count(leaf, subleaf, &regs[0], &regs[1], &regs[3], &regs[4]);
-    return regs[target_reg];
-}
-
-static bool cpu_has_feature(u32 leaf, u32 subleaf, u32 target_reg,
-                            u32 feature_bit) {
-    return hv_utils_is_bit_set(arch_cpuid(leaf, subleaf, target_reg),
-                               feature_bit);
-}
+#define PARSE_SELECTOR(cpu, selector)                                         \
+    hv_arch_read_seg_descriptor(&cpu->state.seg_##selector, cpu->state.gdtr,  \
+                                (SEGMENT_SELECTOR)hv_arch_read_##selector()); \
+    OUTPUT_SEG_DESC(selector, cpu->state.seg_##selector)
 
 bool hv_arch_cpu_has_vmx(void) {
     /* Check if CPUID.1:ECX.VMX[bit 5] = 1. */
-    return cpu_has_feature(CPUID_VMX_ENABLED_LEAF, CPUID_VMX_ENABLED_SUBLEAF,
-                           CPUID_REGISTER_ECX, CPUID_VMX_ENABLED_BIT);
+    return hv_utils_is_bit_set(cpuid_ecx(CPUID_VERSION_INFORMATION),
+                               CPUID_VMX_ENABLED_BIT);
 }
 
-void hv_arch_enable_vmxe() {
-    CR4 cr4 = {.flags = native_read_cr4()};
+void hv_arch_enable_vmxe(void) {
     /* To enable VMX, software must ensure CR4.VMXE[bit 13] = 1. Otherwise,
      * VMXON will generate a #UD exception. */
-    cr4.vmx_enable = 1;
-    native_write_cr4(cr4.flags);
+    cr4_set_bits(X86_CR4_VMXE);
 }
 
 void hv_arch_disable_vmxe(void) {
-    CR4 cr4 = {.flags = native_read_cr4()};
     /* To disable VMX, software must ensure CR4.VMXE[bit 13] = 0. This will
      * cause VMXON to generate a #UD exception. */
-    cr4.vmx_enable = 0;
-    native_write_cr4(cr4.flags);
+    cr4_clear_bits(X86_CR4_VMXE);
 }
 
-u8 hv_arch_do_vmwrite(unsigned long field, unsigned long value) {
+void hv_arch_invd(void) { asm volatile("invd\n"); }
+
+u8 hv_arch_vmwrite(unsigned long field, unsigned long value) {
     u8 ret;
     asm volatile(
         "vmwrite %[value], %[field]\n"
@@ -66,7 +60,16 @@ u8 hv_arch_do_vmwrite(unsigned long field, unsigned long value) {
     return ret;
 }
 
-u8 hv_arch_do_vmclear(phys_addr_t vmcs_region_ptr) {
+u64 hv_arch_vmread(u64 field) {
+    u64 value;
+    asm volatile("vmread %[field], %[value]\n"
+                 : [ value ] "=r"(value)
+                 : [ field ] "r"(field)
+                 : "cc", "memory");
+    return value;
+}
+
+u8 hv_arch_vmclear(phys_addr_t vmcs_region_ptr) {
     u8 ret;
     asm volatile(
         "vmclear %[vmcs_region_ptr]\n"
@@ -77,7 +80,7 @@ u8 hv_arch_do_vmclear(phys_addr_t vmcs_region_ptr) {
     return ret;
 }
 
-u8 hv_arch_do_vmptrld(phys_addr_t vmcs_region_ptr) {
+u8 hv_arch_vmptrld(phys_addr_t vmcs_region_ptr) {
     u8 ret;
     asm volatile(
         "vmptrld %[vmcs_region_ptr]\n"
@@ -88,21 +91,28 @@ u8 hv_arch_do_vmptrld(phys_addr_t vmcs_region_ptr) {
     return ret;
 }
 
-u8 hv_arch_do_vmxoff(void) {
-    u8 cf, zf;
-    /* Takes the logical processor out of VMX operation.
-     * If VMXON failed, CF is set. ZF is set if the VM had a valid failure. */
+u8 hv_arch_vmxoff(void) {
+    u8 cf = 0;
+    u8 zf = 0;
+    /* Takes the logical processor out of VMX operation. If VMXON failed with an
+     * invalid VMCS, CF is set. Otherwise, ZF is set and the error field in the
+     * VMCS is set. */
     asm volatile(
         "vmxoff\n"
         "setb %[cf]\n"
         "setz %[zf]\n"
         : [ cf ] "=rm"(cf), [ zf ] "=rm"(zf)::"cc", "memory");
+
+    if (zf) {
+        hv_utils_cpu_log(err, "VMXOFF failed with VMCS error %llx\n",
+                         hv_arch_vmread(VMCS_VM_INSTRUCTION_ERROR));
+        return 1;
+    }
     return cf | zf;
 }
 
-u8 hv_arch_do_vmxon(phys_addr_t vmxon_region_ptr) {
+u8 hv_arch_vmxon(phys_addr_t vmxon_region_ptr) {
     u8 ret;
-
     /* Puts the logical processor in VMX operation with no current VMCS.
      * If VMXON failed, CF is set. */
     asm volatile(
@@ -115,7 +125,7 @@ u8 hv_arch_do_vmxon(phys_addr_t vmxon_region_ptr) {
     return ret;
 }
 
-u8 hv_arch_do_vmlaunch(void) {
+u8 hv_arch_vmlaunch(void) {
     u8 err;
     asm volatile(
         "vmlaunch\n"
@@ -124,7 +134,7 @@ u8 hv_arch_do_vmlaunch(void) {
     return err;
 }
 
-u64 hv_arch_do_lar(u16 selector) {
+u64 hv_arch_lar(u16 selector) {
     u64 access_rights;
     asm volatile("lar %[access_rights], %[selector]\n"
                  : [ access_rights ] "=r"(access_rights)
@@ -133,7 +143,7 @@ u64 hv_arch_do_lar(u16 selector) {
     return access_rights;
 }
 
-u64 hv_arch_do_lsl(u16 selector) {
+u64 hv_arch_lsl(u16 selector) {
     u64 limit;
     asm volatile("lsl %[limit], %[selector]\n"
                  : [ limit ] "=r"(limit)
@@ -144,29 +154,37 @@ u64 hv_arch_do_lsl(u16 selector) {
 
 u64 hv_arch_read_dr7(void) {
     u64 out;
-    asm volatile("mov %[out], %%dr7\n" : [ out ] "=r"(out));
+    asm volatile("mov %%dr7, %[out]\n" : [ out ] "=r"(out));
     return out;
 }
 
-void hv_arch_do_sldt(SEGMENT_SELECTOR* out) {
+void hv_arch_sdlt(SEGMENT_SELECTOR* out) {
     asm volatile("sldt %[out]\n" : [ out ] "=m"(out));
 }
 
-void hv_arch_capture_cpu_state(struct cpu_ctx* cpu_ctx) {
+void hv_arch_capture_cpu_state(struct cpu_ctx* cpu) {
     SEGMENT_SELECTOR ldtr;
 
-    cpu_ctx->state.regs = *task_pt_regs(get_current());
-    cpu_ctx->state.cr0.flags = native_read_cr0();
-    cpu_ctx->state.cr3.flags = __native_read_cr3();
-    cpu_ctx->state.cr4.flags = native_read_cr4();
-    cpu_ctx->state.dr7.flags = hv_arch_read_dr7();
+    cpu->state.cr0.flags = native_read_cr0();
+    cpu->state.cr3.flags = __read_cr3();
+    cpu->state.cr4.flags = native_read_cr4();
+    cpu->state.dr7.flags = hv_arch_read_dr7();
 
-    native_store_gdt((struct desc_ptr*)&cpu_ctx->state.gdtr);
-    store_idt((struct desc_ptr*)&cpu_ctx->state.idtr);
+    hv_utils_cpu_log(debug, "CR0=%llx, CR3=%llx, CR4=%llx\n",
+                     cpu->state.cr0.flags, cpu->state.cr3.flags,
+                     cpu->state.cr4.flags);
 
-    hv_arch_do_sldt(&ldtr);
-    hv_arch_read_seg_descriptor(&cpu_ctx->state.ldtr, cpu_ctx->state.gdtr,
-                                ldtr);
+    native_store_gdt((struct desc_ptr*)&cpu->state.gdtr);
+    hv_utils_cpu_log(debug, "GDT base=%llx, limit=%x\n",
+                     cpu->state.gdtr.base_address, cpu->state.gdtr.limit);
+
+    store_idt((struct desc_ptr*)&cpu->state.idtr);
+    hv_utils_cpu_log(debug, "IDT base=%llx, limit=%x\n",
+                     cpu->state.idtr.base_address, cpu->state.idtr.limit);
+
+    hv_arch_sdlt(&ldtr);
+    hv_arch_read_seg_descriptor(&cpu->state.ldtr, cpu->state.gdtr, ldtr);
+    OUTPUT_SEG_DESC(LDT, cpu->state.ldtr);
 
     PARSE_SELECTOR(cpu, cs)
     PARSE_SELECTOR(cpu, ds)
@@ -174,18 +192,25 @@ void hv_arch_capture_cpu_state(struct cpu_ctx* cpu_ctx) {
     PARSE_SELECTOR(cpu, ss)
     PARSE_SELECTOR(cpu, gs)
     PARSE_SELECTOR(cpu, fs)
-
-    hv_arch_read_seg_descriptor(&cpu_ctx->state.task_register,
-                                cpu_ctx->state.gdtr,
+    hv_arch_read_seg_descriptor(&cpu->state.task_register, cpu->state.gdtr,
                                 (SEGMENT_SELECTOR)hv_arch_read_tr());
+    OUTPUT_SEG_DESC(tr, cpu->state.task_register)
 
-    cpu_ctx->state.seg_gs_base = native_read_msr(IA32_GS_BASE);
-    cpu_ctx->state.seg_fs_base = native_read_msr(IA32_FS_BASE);
+    cpu->state.seg_gs_base = native_read_msr(IA32_GS_BASE);
+    cpu->state.seg_fs_base = native_read_msr(IA32_FS_BASE);
 
-    cpu_ctx->state.debugctl = native_read_msr(IA32_DEBUGCTL);
-    cpu_ctx->state.sysenter_cs = native_read_msr(IA32_SYSENTER_CS);
-    cpu_ctx->state.sysenter_eip = native_read_msr(IA32_SYSENTER_EIP);
-    cpu_ctx->state.sysenter_esp = native_read_msr(IA32_SYSENTER_ESP);
+    hv_utils_cpu_log(debug, "GS_BASE=%llx, FS_BASE=%llx\n",
+                     cpu->state.seg_gs_base, cpu->state.seg_fs_base);
+
+    cpu->state.debugctl = native_read_msr(IA32_DEBUGCTL);
+    cpu->state.sysenter_cs = native_read_msr(IA32_SYSENTER_CS);
+    cpu->state.sysenter_eip = native_read_msr(IA32_SYSENTER_EIP);
+    cpu->state.sysenter_esp = native_read_msr(IA32_SYSENTER_ESP);
+
+    hv_utils_cpu_log(debug,
+                     "SYSENTER_CS=%llx, SYSENTER_EIP=%llx, SYSENTER_ESP=%llx\n",
+                     cpu->state.sysenter_cs, cpu->state.sysenter_eip,
+                     cpu->state.sysenter_esp);
 }
 
 void hv_arch_read_seg_descriptor(
@@ -193,47 +218,48 @@ void hv_arch_read_seg_descriptor(
     const SEGMENT_DESCRIPTOR_REGISTER_64 dtr, const SEGMENT_SELECTOR selector) {
     SEGMENT_DESCRIPTOR_64 descriptor;
 
-    if (!selector.flags || selector.table) {
-        /* We do not attempt to load a descriptor from the current LDT or load a
-         * NULL segment selector, we instead return an unusable VMX segment. */
+    if (!selector.flags) {
+        /* A NULL selector will attempt to index the first entry of the GDT
+         * which is invalid and therefore unusable. */
         processed_descriptor->access_rights.unusable = 1;
         return;
     }
 
-    /* A segment selector is a 16-bit identifier for a segment. The segment
-     * selector has a 13-bit field at a 3-bit offset. In order to calculate the
-     * descriptor table index, we mask the offset then shift.
-     *
-     * (selector & 0xd) >> 3
-     *
-     * Then, to index into a descriptor table we add the base address of the
-     * descriptor table, and the selector offset multiplied by the size of each
-     * selector (8 bytes).
-     *
-     * base_address + (descriptor_idx << 3)
-     * base_address + (((selector & 0xd) >> 3) << 3)
-     * base_address + (selector & 0xd)
-     * */
     descriptor =
-        *(SEGMENT_DESCRIPTOR_64*)(dtr.base_address + (selector.flags & 0xd));
+        *(SEGMENT_DESCRIPTOR_64*)(dtr.base_address + (selector.index << 3));
 
     memset(processed_descriptor, 0x0,
            sizeof(struct hv_arch_segment_descriptor));
 
-    processed_descriptor->selector = selector;
-    processed_descriptor->dtr = dtr;
-    processed_descriptor->limit = hv_arch_do_lsl(selector.flags);
-    processed_descriptor->access_rights.flags = hv_arch_do_lar(selector.flags);
+    /* Clear RPL and TI flags for VMCS host state fields to be valid. */
+    processed_descriptor->selector.flags = selector.flags & ~0x7;
+    processed_descriptor->limit = hv_arch_lsl(selector.flags);
+
+    /* LAR does not yield the correct result, I am unsure why. Hence, I load
+     * the access rights manually. */
+    processed_descriptor->access_rights.type = descriptor.type;
+    processed_descriptor->access_rights.descriptor_type =
+        descriptor.descriptor_type;
+    processed_descriptor->access_rights.descriptor_privilege_level =
+        descriptor.descriptor_privilege_level;
+    processed_descriptor->access_rights.present = descriptor.present;
+    processed_descriptor->access_rights.available_bit = descriptor.system;
+    processed_descriptor->access_rights.long_mode = descriptor.long_mode;
+    processed_descriptor->access_rights.default_big = descriptor.default_big;
+    processed_descriptor->access_rights.granularity = descriptor.granularity;
 
     processed_descriptor->base_address =
         descriptor.base_address_low | (descriptor.base_address_middle << 16) |
         (descriptor.base_address_high << 24);
 
-    if (!descriptor.system) {
+    if (!descriptor.descriptor_type &&
+        (descriptor.type == SEGMENT_DESCRIPTOR_TYPE_LDT ||
+         descriptor.type == SEGMENT_DESCRIPTOR_TYPE_TSS_AVAILABLE ||
+         descriptor.type == SEGMENT_DESCRIPTOR_TYPE_TSS_BUSY)) {
         /* System descriptors are expanded to 16 byte descriptors which can hold
          * 64-bit addresses, we must include the upper address segment. */
         processed_descriptor->base_address |= (u64)descriptor.base_address_upper
-                                              << 32L;
+                                              << 32UL;
     }
 
     /* Attempts to use a VMX segment with the unusable bit (bit 16) will fault.
