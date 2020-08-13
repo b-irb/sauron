f49e6e0d7d78fdc5cc126e063b75097a6a8ec05a
birb007 <25516647+birb007@users.noreply.github.com>
2020-08-13 21:47:40 +0100

    implement struct to access guest registers

--- src/vmxasm.h
+++ src/vmxasm.h
@@ -1,4 +1,26 @@
 #pragma once
 
+#include <linux/types.h>
+
 extern void hv_cpu_init_entry(void*);
-extern void hv_vmx_guest_resume(void);
+extern void* hv_exit_vmexit_entry;
+
+struct hv_exit_state {
+    u64 r15;
+    u64 r14;
+    u64 r13;
+    u64 r12;
+    u64 r11;
+    u64 r10;
+    u64 r9;
+    u64 r8;
+    u64 rdi;
+    u64 rsi;
+    u64 rdx;
+    u64 rcx;
+    u64 rbx;
+    u64 rax;
+    /* RSP must be read from the VMCS within the vmexit handler. */
+    u64 _padding;
+    u64 rbp;
+} __attribute__((aligned(16)));
